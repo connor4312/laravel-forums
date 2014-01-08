bd09d3009dc74f135d2ff9d769f1debf60b0f058
Connor Peet <connor@connorpeet.com>
2014-01-07 22:47:48 -0600

    'Initial'

---
+++ src/Connor4312/LaravelForums/LaravelForumsServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Connor4312\LaravelForums;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelForumsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('connor4312/laravel-forums');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
